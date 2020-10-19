1f370d9345bf7d4878cb5e091c49b21938d1025a
aalagic <aldin_alagic@hotmail.com>
2020-10-20 01:12:10 +0200

    Create a .NET core console application project

---
+++ aalagic_zadaca_1/aalagic_zadaca_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aalagic_zadaca_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
